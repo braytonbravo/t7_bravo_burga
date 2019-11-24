70fed994f1163ed5a15d5785f8ffec8e3dcb4bd0
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-11-24 09:32:11 -0500

    se desarrollo bucle_para_1.py

---
+++ Bravo/bucle_para_1.py
@@ -0,0 +1,22 @@
+#DECODIFICAR MENSAJE ENCRIPTADO
+# a= hola
+# e= buenos dias
+# i=que tal
+# o=que haces
+# u=mañana a clases
+MSG="aeiou"
+for letra in MSG:
+    if letra == "a":
+        print("hola")
+    if letra == "e":
+        print("buenos dias")
+    if letra == "i":
+        print("que tal")
+    if letra == "o":
+        print("que haces")
+    if letra == "u":
+        print("mañana a clases")
+#fin_interador
+
+print("fin del bucle")
+
