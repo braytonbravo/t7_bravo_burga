df35fe3e9970c266d85d710769a22ac66b95b8e8
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-11-24 09:38:47 -0500

    se desarrollo bucle_para_5.py

---
+++ Bravo/bucle_para_5.py
@@ -0,0 +1,22 @@
+#DECODIFICAR MENSAJE ENCRIPTADO
+# B=Bueno
+# R=Respetuoso
+# A=Amable
+# V=Valiente
+# 0=Optimista
+MSG="BRAVO"
+for letra in MSG:
+    if letra == "B":
+        print("Bueno")
+    if letra == "R":
+        print("Respetuoso")
+    if letra == "A":
+        print("Amable")
+    if letra == "V":
+        print("Valiente")
+    if letra == "O":
+        print("Optimista")
+#fin_interador
+
+print("fin del bucle")
+
