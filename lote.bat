9c0b265e43d510759dadc374c6ff79aaf63d266c
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-11-24 13:27:13 -0500

    se desarrollo lote.bat

---
+++ lote.bat
@@ -0,0 +1,46 @@
+python Bravo/validador1.py    35
+python Bravo/validador2.py    10
+python Bravo/validador3.py    120
+python Bravo/validador4.py    14.5
+python Bravo/validador5.py    53.41
+python Burga/validador6.py    60
+python Burga/validador7.py    28
+python Burga/validador8.py    56
+python Burga/validador9.py    190
+python Burga/validador10.py   33
+
+
+python Bravo/interacion1.py    1002001
+python Bravo/interacion2.py    120
+python Bravo/interacion3.py    110
+python Bravo/interacion4.py    198
+python Bravo/interacion5.py    4905
+python Burga/interacion6.py
+python Burga/interacion7.py
+python Burga/interacion8.py
+python Burga/interacion9.py
+python Burga/interacion10.py
+
+
+python Bravo/bucle_para_1.py    "aeiou"
+python Bravo/bucle_para_2.py    "ANO"
+python Bravo/bucle_para_3.py    "FACFYM"
+python Bravo/bucle_para_4.py    "MRUV"
+python Bravo/bucle_para_5.py    "BRAVO"
+python Burga/bucle_para_6.py    "AABFR"
+python Burga/bucle_para_7.py    "absw"
+python Burga/bucle_para_8.py    "efgj"
+python Burga/bucle_para_9.py    "erhl"
+python Burga/bucle_para_10.py   "nmps"
+
+
+python Bravo/for_range_1.py      5
+python Bravo/for_range_2.py      180
+python Bravo/for_range_3.py      -2
+python Bravo/for_range_4.py      0
+python Bravo/for_range_5.py      999
+python Burga/for_range_6.py      30
+python Burga/for_range_7.py      34
+python Burga/for_range_8.py      19
+python Burga/for_range_9.py      25
+python Burga/for_range_10.py     10
