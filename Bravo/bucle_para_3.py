86a95ad5e6ad9bdcb558bb995ae95c8359fa1399
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-11-24 09:33:43 -0500

    se desarrollo bucle_para_3.py

---
+++ Bravo/bucle_para_3.py
@@ -0,0 +1,22 @@
+#DECODIFICAR MENSAJE ENCRIPTADO
+# FA=Facultad
+# C=de Ciencias
+# F=Fisicas
+# Y=y
+# M=Matematicas
+MSG="FACFYM"
+for letra in MSG:
+    if letra == "FA":
+        print("Facultad")
+    if letra == "C":
+        print("de Ciencias")
+    if letra == "F":
+        print("Fisicas")
+    if letra == "Y":
+        print("y")
+    if letra == "M":
+        print("Matematicas")
+#fin_interador
+
+print("fin del bucle")
+
