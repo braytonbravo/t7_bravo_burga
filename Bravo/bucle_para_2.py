e8723d1700e9dc7a082b55c89dfd07d6b380709e
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-11-24 09:33:01 -0500

    se desarrollo bucle_para_2.py

---
+++ Bravo/bucle_para_2.py
@@ -0,0 +1,18 @@
+#DECODIFICAR MENSAJE ENCRIPTADO
+# 2=Ano
+# 0=de la lucha
+# 1=contra la coruccion
+# 9=e impunidad
+MSG=2019
+for numero in MSG:
+    if numero == 2:
+        print("Ano")
+    if numero == 0:
+        print("de la lucha")
+    if numero == 1:
+        print("contra la corrupcion")
+    if numero == 9:
+        print("e impunidad")
+#fin_interador
+
+print("fin del bucle")
