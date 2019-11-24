b2bbc64391fc40a711f9e5c16556f97d106fa8e5
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-11-24 09:18:30 -0500

    se desarrollado interacion4.py

---
+++ Bravo/interacion4.py
@@ -0,0 +1,8 @@
+#sumar los 5 primeros numeros que terminan en 8
+i=8
+suma=0
+while (i<=58):
+    suma +=i
+    i +=10
+#fin_while
+print("la suma de los 5 primeros numeros que terminan en 8:", suma)
