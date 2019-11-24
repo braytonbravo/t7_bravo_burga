ea926d2b06a94f8acc94b5a63ea1840945bef0ed
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-11-24 09:34:24 -0500

    se desarrollo bucle_para_4.py

---
+++ Bravo/bucle_para_4.py
@@ -0,0 +1,18 @@
+#DECODIFICAR MENSAJE ENCRIPTADO
+# M=Movimiento
+# R=Rectilineo
+# U=Uniformemente
+# V=Variado
+MSG="MRUV"
+for letra in MSG:
+    if letra == "M":
+        print("Movimiento")
+    if letra == "R":
+        print("Rectilineo")
+    if letra == "U":
+        print("Uniformemente")
+    if letra == "V":
+        print("Variado")
+#fin_interador
+
+print("fin del bucle")
